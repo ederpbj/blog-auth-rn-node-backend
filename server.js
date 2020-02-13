e7c718e0cdc128034f87506b1df629977dc304cc
ederpbj <ederpbj@yahoo.com.br>
2020-02-12 21:38:52 -0300

    Corrigindo problema de conexão mongo

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 
-// Conecta no MongoDB
-mongoose.connect(
+// Conecta no MongoDB, não funciona
+/* mongoose.connect(
   "mongodb://dev:e609f4f6ac550e8d96b64f835bd36fad@ds135750.mlab.com:35750/auth-jwt-node-react-native"
-);
+); */
+
+//T1
+const url =
+  "mongodb://dev:e609f4f6ac550e8d96b64f835bd36fad@ds135750.mlab.com:35750/auth-jwt-node-react-native";
+
+const mongoose = require('mongoose'); mongoose.set('useCreateIndex', true);
+
+mongoose.connect(url, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true})
+  .then(() => console.log("Connected to Database") )
+  .catch(err => console.error("An error has occured", err));
+//<
 
 // Carrega o model de Usuário
 require("./models/user");
